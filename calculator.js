fe68744f61e0012d7a8966ed718491b33b61f86d
Jessica Unruh <junruh@Jessicas-MacBook-Air.local>
2023-01-25 17:36:47 -0800

    Basic file setup
    Adds the main files for the website.

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// functionality for calculator:
+// add, subtract, multiply, divide, equals, clear, numbers, decimals
+// all can be used interchangeably
+var number = 0;
+console.log("Hello World!");
+function addCalc(number) {
+}
+function subCalc(number) {
+}
+function multiplyCalc(number) {
+}
+function divideCalc(number) {
+}
+function equalsCalc(number) {
+}
+function clearCalc(number) {
+}
---
+++ calculator.ts
@@ -0,0 +1,29 @@
+// functionality for calculator:
+// add, subtract, multiply, divide, equals, clear, numbers, decimals
+// all can be used interchangeably
+
+let number: number = 0;
+
+function addCalc(number){
+
+}
+
+function subCalc(number){
+
+}
+
+function multiplyCalc(number){
+
+}
+
+function divideCalc(number){
+
+}
+
+function equalsCalc(number){
+
+}
+
+function clearCalc(number){
+
+}
