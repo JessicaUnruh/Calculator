294adc368ac7406407adc01fb0257dafa8a5d95d
Jessica Unruh <junruh@Jessicas-MacBook-Air.local>
2023-03-06 16:14:43 -0800

    Layout and Bootstrap implementation
    Added buttons inside a row and column set up, as well as adding in icons and a minor CSS edit.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,21 @@
 // functionality for calculator:
 // add, subtract, multiply, divide, equals, clear, numbers, decimals
 // all can be used interchangeably
-var number = 0;
-console.log("Hello World!");
-function addCalc(number) {
+// user can input number only
+// check for if input is number; if not they must re-enter the input
+var number = 0, buttonPress, operationButton, result;
+console.log(5 / 2);
+function caculateExpression(number) {
+    switch (operationButton) {
+        case operationButton = "add":
+            result == number + buttonPress;
+        case operationButton = "subtract":
+            result == number - buttonPress;
+        case operationButton = "multiply":
+            result == number * buttonPress;
+        case operationButton = "divide":
+            result == number / buttonPress;
+    }
 }
 function subCalc(number) {
 }
--- calculator.ts
+++ calculator.ts
@@ -2,28 +2,32 @@
 // add, subtract, multiply, divide, equals, clear, numbers, decimals
 // all can be used interchangeably
 
-let number: number = 0;
+// user can input number only
+// check for if input is number; if not they must re-enter the input
 
-function addCalc(number){
+let number: number = 0,
+    buttonPress: number,
+    operationButton: string,
+    result: number;
 
-}
+function caculateExpression(number){
+    switch (operationButton){
+        case operationButton = "add":
+            result == number + buttonPress;
+            console.log(result);
 
-function subCalc(number){
+        case operationButton = "subtract":
+            result == number - buttonPress;
+            console.log(result);
 
-}
+        case operationButton = "multiply":
+            result == number * buttonPress;
+            console.log(result);
+            
+        case operationButton = "divide":
+            result == number / buttonPress;
+            console.log(result);
 
-function multiplyCalc(number){
-
-}
-
-function divideCalc(number){
-
-}
-
-function equalsCalc(number){
-
-}
-
-function clearCalc(number){
-
+        
+    }
 }
